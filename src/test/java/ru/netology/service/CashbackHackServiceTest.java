9180747d18b165af93787ad26c548870de64f8e6
Alexeybmw <109792704+Alexeybmw@users.noreply.github.com>
2023-05-15 11:28:02 +0300

    junit.jupiter

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -31,4 +31,32 @@ public class CashbackHackServiceTest {
         org.junit.Assert.assertEquals(actual, expected);
 
     }
+    @org.junit.jupiter.api.Test
+    public void testRemain_0() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 500;
+        int expected = 500;
+        int actual = cashbackHackService.remain(amount);
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
+
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testRemain_1() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1500;
+        int expected = 500;
+        int actual = cashbackHackService.remain(amount);
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
+
+    }
+    @org.junit.jupiter.api.Test
+    public void testRemain_2() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int expected = 0;
+        int actual = cashbackHackService.remain(amount);
+        org.junit.jupiter.api.Assertions.assertEquals(actual, expected);
+
+    }
 }
